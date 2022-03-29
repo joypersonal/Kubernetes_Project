f38fed0fef2460aed461badd16b1bc442bed17e0
joypersonal <joypersonal1981@gmail.com>
2022-03-29 19:39:42 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -31,4 +31,4 @@ def details():
     return render_template('index.html', HOSTNAME=hostname)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(host='0.0.0.0', port=5000)
